7525ed4a45205cc4ec67abc7b8cf1508dbafc234
zhangtonglei <zhangtonglei@tidebuy.net>
2017-10-27 17:22:15 +0800

    1027 归档显示

--- src/pages/archives/index.js
+++ src/pages/archives/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {
-    Link
+    Link,
 } from 'react-router-dom'
 import {connect} from 'react-redux';
 import {getArchives} from './action';
@@ -10,30 +10,42 @@ class Archives extends Component {
     constructor(props){
         super(props);
         this.state = {
-            archives: [],
+            archives: this.props.archives||[],
+            archiveItme:[],
         }
     }
     componentDidMount(){
         this.props.dispatch(getArchives());
+
+
     }
     componentWillReceiveProps(nextProps){
-        //todo 处理显示每个archive的数据
-        let archives = this.props.archives;
+        /*let archives = this.props.archives;
         if(nextProps.match.path != '/archive') {
 
         }
 
         this.setState({
             archives,
+        })*/
+
+
+        let arr = [];
+        let obj = {};
+        this.props.archives.map((v, i)=>{
+            if(!obj[v.archive]){
+                obj[v.archive] = 1;
+                arr.push(v.archive);
+            }
+        });
+        this.setState({
+            archiveItme:arr,
         })
     }
     fliterArchives = (archive) =>{
-        let archives = this.props.archives;
-        if(true) {
-            archives = this.props.archives.filter((v, i)=>{
+        let archives = this.props.archives.filter((v, i)=>{
                 return v.archive === archive
             })
-        }
 
         this.setState({
             archives,
@@ -41,20 +53,23 @@ class Archives extends Component {
     }
     render(){
         //console.log(this.props)
-        //todo 提取archive
         return(
             <div className='archive'>
                 <ul className='left'>
                     <li>
                         <h3>Archives:</h3>
                     </li>
-                    <li>
-                        <Link to='/'>aaaaaaa111</Link>
-                    </li>
+                    {
+                        this.state.archiveItme.map((v,i)=>{
+                           return  <li key={v} onClick={()=>this.fliterArchives(v)}>
+                                        <Link to={`#${v}`} style={this.props.location.hash===`#${v}` ? {color:'#e9cd4c'}:{}}>{v}</Link>
+                                    </li>
+                        })
+                    }
                 </ul>
                 <ul className='right'>
                     {
-                        this.props.archives.map((v,i)=>{
+                        this.state.archives.map((v,i)=>{
                           return   <li key={v.time}>
                                 <Link to={`${this.props.match.url}/${v.time}`} className="title">
                                     <span className='time'>{v.date}</span>{v.title}
